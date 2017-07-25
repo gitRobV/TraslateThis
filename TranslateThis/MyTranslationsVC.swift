6840cfc6e2a62282c620389b9dae7feb6af4cdaa
Ruben Duran <rd@rubenduran.net>
2017-07-25 12:54:20 -0700

    Added My Translations VC to story board

---
+++ TranslateThis/MyTranslationsVC.swift
@@ -0,0 +1,27 @@
+//
+//  MyTranslationsVC.swift
+//  TranslateThis
+//
+//  Created by Ruben Duran on 7/25/17.
+//  Copyright © 2017 Robert Villarreal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class MyTranslationsVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
